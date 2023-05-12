511ff2f1934864f93ca57a353ef14dbae9d2dd8d
jose.drumond <carlospientznauer@gmail.com>
2023-05-12 00:39:14 -0300

    Alteração de nome de usuário

--- app/src/main/java/com/jose/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/jose/meuprimeiroapp/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         var senha = findViewById<EditText>(senha)
         var mensagemLogin = findViewById<TextView>(mensagemLogin)
         botao.setOnClickListener {
-            if (usuario.text.toString() != "Drumond" || senha.text.toString() != "8009"){
+            if (usuario.text.toString() != "Drumond1" || senha.text.toString() != "8009"){
                 mensagemLogin.isVisible = true
                 mensagemLogin.text = "Usuário ou senha incorretos."
             }
